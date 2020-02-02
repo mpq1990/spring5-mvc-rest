47069b40e4886cffa241702cbd4726042bc3a6ff
Majid Qureshi <mpq1990@gmail.com>
2020-02-02 22:45:33 +0100

    chore: add metadata

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -19,6 +23,16 @@ public class SwaggerConfig {
                 apis(RequestHandlerSelectors.any()).
                 paths(PathSelectors.any()).
                 build().
-                pathMapping("/");
+                pathMapping("/").
+                apiInfo(metaData());
     }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Test", "https://www.google.com", "test@test.de");
+
+        return new ApiInfo("Awesome Api", "Demo rest app", "1.0", "what eva",
+                contact, "Apache Licence Version 2.0", "google it", new ArrayList<>());
+    }
+
+
 }
